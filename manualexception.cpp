a94b21170665b60cd8d5dc5cc8bf076887d8c70e
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-06-04 15:24:35 +0700

    menambahkan catch

--- manualexception.cpp
+++ manualexception.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-
+    try {
+        cout << "Selamat Belajar Di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak dieksekusi" << endl;
+    }
+    catch(int a){
+        //blok ini di eksekusi 
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch(...) {
+        /*jika selain integer maka block ini dieksekusi*/
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
